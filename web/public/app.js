c35c135075d103699e80d08db9d695d799791518
Jerome Goldstein <jeromegold@gmail.com>
2018-09-03 13:51:03 +1000

    NEW: Created PopUp and MapMarker

--- web/public/app.js
+++ web/public/app.js
@@ -331,3 +331,80 @@ alert("The password is incorrect!");
 
 });
 */
+
+
+var map;
+/* mobilityArray is the alcoholArray and is the older people*/
+var mobilityArray = [];
+
+/* mobilityMarkers is the mobility marker (acohol) */
+
+var mobilityMarkers = [];
+
+
+var countMobilitySelected = 0;
+
+
+function addMobilityMarkerFirstTime() {
+
+
+  console.log(mobilityArray.length);
+
+  for (i = 0; i < mobilityArray.length; i++) {
+
+    var latFloat, lngFloat;
+    latFloat = parseFloat(mobilityArray[i]["lat"]);
+    lngFloat = parseFloat(mobilityArray[i]["lng"]);
+
+    console.log(latFloat);
+    console.log(lngFloat);
+
+    var marker_position = {lat: latFloat, lng: lngFloat};
+    var marker = new google.maps.Marker({
+      position: marker_position,
+      map: map,
+      icon: 'images/personicon.png',
+      customData: mobilityArray[i]
+    });
+
+    mobilityMarkers.push(marker);
+
+    marker.addListener('click', function() {
+
+      open_pop_up(this.customData);
+    });
+
+  }
+}
+
+function open_pop_up(mobility_object){
+
+
+  var open_pop = document.getElementById('pop_up');
+  open_pop.style.display = 'block';
+  var text_name = document.getElementById("personName");
+  text_name.innerHTML = mobility_object["uid"];
+
+
+}
+
+function close_pop_up(){
+
+  var close_pop = document.getElementById('pop_up');
+  close_pop.style.display = 'none';
+}
+
+
+function readInMobility()
+{
+  db.collection("data").get().then((querySnapshot) => {
+
+    querySnapshot.forEach((doc) => {
+
+      mobilityArray.push(doc.data());
+
+    });
+
+    addMobilityMarkerFirstTime();
+  });
+}
