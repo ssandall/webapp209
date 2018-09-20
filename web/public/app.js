d55688b1803cda0c21ecebcad5dee7f113bbee82
Jerome Goldstein <jeromegold@gmail.com>
2018-09-20 21:58:12 +1000

    UPDATED: redirect to map on login

--- web/public/app.js
+++ web/public/app.js
@@ -140,6 +140,6 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-//  window.location.href = 'http://localhost:3000/map.html';
+  window.location.href = 'http://localhost:3000/map.html';
 
 }
